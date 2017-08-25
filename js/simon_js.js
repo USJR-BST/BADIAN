8bf1db93b1bddec259e7340873285d95cfa4c66e
USJR-BST <usjr.bst@gmail.com>
2017-08-25 15:30:10 +0800

    Updated ATF Added Animations in Logo(mobile view) and brandstamp

--- js/simon_js.js
+++ js/simon_js.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+    $('.parallax1').parallax();
+
+    if (window.matchMedia('(max-width: 979px)').matches) {
+        $(window).scroll(function()
+        {
+            var div = $(this);
+            // if (div.scrollHeight - div.scrollTop() === div.height())
+            // {
+            //     $(".brand-logo img").removeClass(" animated zoomIn");
+            //     $(".brand-logo img").addClass(" animated zoomOut");
+            //     alert("bottom");
+            // }
+            // else
+            if(div.scrollTop() === 0)
+            {
+                $(".brand-logo img").removeClass(" animated zoomOut");
+                $(".brand-logo img").addClass(" animated zoomIn");
+            }else{
+                $(".brand-logo img").removeClass(" animated zoomIn");
+                $(".brand-logo img").addClass(" animated zoomOut");
+            }
+        });
+    }
+
 });
